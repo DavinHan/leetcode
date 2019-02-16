5127eb68915fe234aa8a7268a2bbe3b9c3de3a5b
han666000@126.com <han666000@126.com>
2019-02-16 10:26:12 +0800

    leetcode

--- leetcode-training/src/main/java/com/test/MaxIncreaseToKeepCitySkyline.java
+++ leetcode-training/src/main/java/com/test/MaxIncreaseToKeepCitySkyline.java
@@ -4,15 +4,15 @@ public class MaxIncreaseToKeepCitySkyline {
 	
 	public static void main(String[] args) {
 		int [][]a = {{3,0,8,4},{2,4,5,7},{9,2,6,3},{0,3,1,0}};
-		for(int i = 0;i < 4;i++) {
-			for(int j = 0;j < 4;j++) {
-				System.out.println(a[i][j]);
-			}
-		}
+		System.out.println(maxIncreaseKeepingSkyline(a));
 	}
 	
 	public static int maxIncreaseKeepingSkyline(int[][] grid) {
-        
+        for (int[] a : grid) {
+			for (int b : a) {
+				
+			}
+		}
 		return 0;
     }
 }
