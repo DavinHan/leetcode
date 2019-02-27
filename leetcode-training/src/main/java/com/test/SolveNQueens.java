176e408cda826d97c962d0eea74f45879ae6a728
han666000@126.com <han666000@126.com>
2019-02-27 19:06:58 +0800

    push

---
+++ leetcode-training/src/main/java/com/test/SolveNQueens.java
@@ -0,0 +1,15 @@
+package com.test;
+
+import java.util.List;
+
+public class SolveNQueens {
+	
+	public static void main(String[] args) {
+		System.out.println(solveNQueens(4));
+	}
+	
+	public static List<List<String>> solveNQueens(int n){
+		
+		return null;
+	}
+}
