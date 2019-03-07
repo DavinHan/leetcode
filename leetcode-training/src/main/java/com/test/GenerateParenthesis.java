d28adabecb7984212e35fd102a172a3ee569ecc4
han666000@126.com <han666000@126.com>
2019-03-07 14:13:54 +0800

    简单旋转矩阵

---
+++ leetcode-training/src/main/java/com/test/GenerateParenthesis.java
@@ -0,0 +1,25 @@
+package com.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateParenthesis {
+	
+	public static void main(String[] args) {
+		int n = 3;
+		List<String> res = generateParenthesis(n);
+		for(String result : res) {
+			System.out.println(result);
+		}
+	}
+	
+	public static List<String> generateParenthesis(int n) {
+		List<String> res = new ArrayList<>();
+		return res;
+	}
+	
+	public static String function(String a, int n){
+		
+		return "";
+	}
+}
