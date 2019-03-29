cc4e33b0726ad6cbfce9709bbd580c9aeacc6b31
han666000@126.com <han666000@126.com>
2019-03-29 19:24:19 +0800

    fix

--- leetcode-training/src/main/java/com/test/middle/Combine.java
+++ leetcode-training/src/main/java/com/test/middle/Combine.java
@@ -7,7 +7,7 @@ public class Combine {
 	
 	public static void main(String[] args) {
 		Combine c = new Combine();
-		List<List<Integer>> res = c.combine(4, 2);
+		List<List<Integer>> res = c.combine(20, 16);
 		for(int i = 0;i < res.size();i++) {
 			String tmp = "";
 			for(int j = 0;j < res.get(i).size();j++) {
@@ -17,14 +17,29 @@ public class Combine {
 		}
 	}
 	
+	//TODO 未完成超时测试
 	public List<List<Integer>> combine(int n, int k) {
 		List<List<Integer>> res = new ArrayList<>();
-		
+		for(int i = 1;i <= n;i++) {
+			List<Integer> list = new ArrayList<>();
+			list.add(i);
+			res.add(list);
+		}
+		if(!res.isEmpty()) {
+			while(res.get(0).size() < k) {
+				List<Integer> l = res.remove(0);
+				for(int i = l.get(l.size() - 1) + 1;i <= n;i++) {
+					if(!l.contains(i)) {
+						List<Integer> tmp = new ArrayList<>();
+						tmp.addAll(l);
+						tmp.add(i);
+						if(!res.contains(tmp)) {
+							res.add(tmp);
+						}
+					}
+				}
+			}
+		}
 		return res;
-	}
-	
-	public List<List<Integer>> function(int n, int k,List<List<Integer>> list){
-		
-		return null;
 	}
 }
