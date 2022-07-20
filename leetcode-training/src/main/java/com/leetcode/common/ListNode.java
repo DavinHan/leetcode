b5ea132f894b50badb1839a0b1cd9d44633764a6
yanhanf <yanhanf@yonyou.com>
2022-07-20 10:47:11 +0800

    feat(algorithms): algorithms
    补充方法用数组创建链表

--- leetcode-training/src/main/java/com/leetcode/common/ListNode.java
+++ leetcode-training/src/main/java/com/leetcode/common/ListNode.java
@@ -8,6 +8,20 @@ public class ListNode {
     public ListNode(int val) { this.val = val; }
     public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 
+    public ListNode(int[] arr) {
+        ListNode root = this;
+        if(arr.length > 0) {
+          ListNode p = root;
+          for(int i = 0;i < arr.length;i++) {
+              p.val = arr[i];
+              if(i < arr.length - 1) {
+                  p.next = new ListNode();
+                  p = p.next;
+              }
+          }
+        }
+    }
+
     @Override
     public String toString() {
         return val + (next == null ? "" : ", " + this.next.toString());
