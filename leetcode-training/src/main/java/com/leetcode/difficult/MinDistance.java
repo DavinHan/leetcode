6c1984a83593f8720a55f31e3e661be33ac9735c
yanhanf <yanhanf@yonyou.com>
2022-07-27 14:58:39 +0800

    feat(algorithms): algorithms
    MinDistance

--- leetcode-training/src/main/java/com/leetcode/difficult/MinDistance.java
+++ leetcode-training/src/main/java/com/leetcode/difficult/MinDistance.java
@@ -1,8 +1,5 @@
 package com.leetcode.difficult;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * 给你两个单词 word1 和 word2， 请返回将 word1 转换成 word2 所使用的最少操作数 。 
  * 你可以对一个单词进行如下三种操作： 
@@ -18,9 +15,22 @@ public class MinDistance {
     public int minDistance(String word1, String word2) {
         char[] c1 = word1.toCharArray();
         char[] c2 = word2.toCharArray();
-        List<String> mcs = new ArrayList<>();
-
-        return 0;
+        int[][] cm = new int[c1.length + 1][c2.length + 1];
+        for(int i = 0;i < cm.length;i++) {
+            cm[i][0] = i;
+        }
+        for(int j = 0;j < cm[0].length;j++) {
+            cm[0][j] = j;
+        }
+        for(int i = 1;i < cm.length;i++) {
+            for(int j = 1;j < cm[0].length;j++) {
+                cm[i][j] = Math.min(Math.min(cm[i][j - 1], cm[i - 1][j]), cm[i - 1][j - 1]) + 1;
+                if(c1[i - 1] == c2[j - 1]) {
+                    cm[i][j] = Math.min(cm[i][j], cm[i - 1][j - 1]);
+                }
+            }
+        }
+        return cm[c1.length][c2.length];
     }
 
     public static void main(String[] args) {
@@ -41,9 +51,5 @@ public class MinDistance {
          * execution
          */
         System.out.println("target is 5, and test is " + d.minDistance("intention", "execution"));
-
-
-
-
     }
 }
