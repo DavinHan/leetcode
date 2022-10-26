a1b9b715e172ec888a72551443479c16470ea5f4
yanhanf <yanhanf@yonyou.com>
2022-10-26 10:31:24 +0800

    fix(project): project module
    fix project module

--- leetcode-training/src/main/java/com/leetcode/common/TreeNode.java
+++ leetcode-training/src/main/java/com/leetcode/common/TreeNode.java
@@ -1,7 +1,7 @@
 package com.leetcode.common;
 
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * TreeNode
@@ -29,5 +29,35 @@ public class TreeNode {
         return this.val + (this.left == null && this.right == null ? "" : ","
                 + (this.left == null ? "null" : left.toString()) + ","
                 + (this.right == null ? "null" : right.toString()));
+    }
+
+    /**
+     * 1,2,2,3,4,4,3
+     */
+    public static TreeNode buildTree(Integer[] nodes) {
+        TreeNode root = new TreeNode();
+        if(nodes.length > 0) {
+            root.val = nodes[0];
+        }
+        List<TreeNode> l = new ArrayList<>();;
+        l.add(root);
+        int curr = 0;
+        for (int i = 1;i < nodes.length;i += 2) {
+            TreeNode p = l.get(curr++);
+            if(p == null) {
+                break;
+            }
+            if(nodes[i] != null) {
+                p.left = new TreeNode();
+                p.left.val = nodes[i];
+                l.add(p.left);
+            }
+            if(i + 1 < nodes.length && nodes[i + 1] != null) {
+                p.right = new TreeNode();
+                p.right.val = nodes[i + 1];
+                l.add(p.right);
+            }
+        }
+        return root;
     }
 }
