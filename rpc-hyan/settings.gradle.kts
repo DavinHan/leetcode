2cae5c8edb81ec5b7a79713779eb942b2c5f9318
yanhanf <yanhanf@yonyou.com>
2022-12-20 17:07:57 +0800

    remove rpc-demo

--- rpc-hyan/settings.gradle.kts
+++
@@ -1,8 +0,0 @@
-rootProject.name = "rpc-hyan"
-
-pluginManagement {
-    plugins {
-        id("org.springframework.boot") version "2.5.11"
-        id("io.spring.dependency-management") version "1.0.10.RELEASE"
-    }
-}
